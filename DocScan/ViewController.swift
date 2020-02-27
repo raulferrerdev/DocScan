275f64bdce5f46a12259473d316756795b9852e2
raulferrerdev <raul.ferrer.dev@gmail.com>
2020-02-27 14:20:49 +0100

    Initial Commit

---
+++ DocScan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DocScan
+//
+//  Created by RaulF on 27/02/2020.
+//  Copyright © 2020 ImTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
