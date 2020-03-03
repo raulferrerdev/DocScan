6b5c9dc23ee4f652f50f1f097e88939f68651ff6
raulferrerdev <raul.ferrer.dev@gmail.com>
2020-03-03 08:09:29 +0100

    Added Scan button

---
+++ DocScan/ScanButton.swift
@@ -0,0 +1,32 @@
+//
+//  ScanButton.swift
+//  DocScan
+//
+//  Created by RaulF on 03/03/2020.
+//  Copyright © 2020 ImTech. All rights reserved.
+//
+
+import UIKit
+
+class ScanButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        setTitle("Scan", for: .normal)
+        titleLabel?.font = UIFont.boldSystemFont(ofSize: 18.0)
+        titleLabel?.textColor = .white
+        layer.cornerRadius = 7.0
+        backgroundColor = UIColor.systemIndigo
+    }
+}
---
+++ DocScan/ScanImageView.swift
@@ -0,0 +1,21 @@
+//
+//  ScanImageView.swift
+//  DocScan
+//
+//  Created by RaulF on 03/03/2020.
+//  Copyright © 2020 ImTech. All rights reserved.
+//
+
+import UIKit
+
+class ScanImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
